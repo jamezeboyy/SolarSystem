4c042c86b7cbdd8cea9c9343449e1667deb2e7cc
Duncan Jones <dj223@outlook.com>
2024-07-10 22:39:08 +0100

    Added GLAD loader

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,11 @@
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 #include <iostream>
+
+constexpr int WIDTH = 640;
+constexpr int HEIGHT = 480;
+
 
 int main() {
     // Initialize the library
@@ -8,24 +13,41 @@ int main() {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-
     // Make the window's context current
     glfwMakeContextCurrent(window);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
         // Render here
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
         // Swap front and back buffers
+        // See https://stackoverflow.com/a/6544453 about doubling buffering
         glfwSwapBuffers(window);
 
         // Poll for and process events
@@ -34,11 +56,7 @@ int main() {
 
     glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
 
-
-void test()
-{
-
-}
