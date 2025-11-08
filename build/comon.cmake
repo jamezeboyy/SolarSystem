3ad7f6fb9e84588b75011061de6bf4dde8716e79
Duncan Jones <duncan.da.jones@gmail.com>
2025-11-08 15:49:33 +0000

    Fixed window specific cmake changes

--- build/comon.cmake
+++ build/comon.cmake
@@ -1,18 +1,22 @@
 
 
-
-if(WIN32)
-    include(${PROJECT_SOURCE_DIR}/build/windows.cmake)
-else()
-    include(${PROJECT_SOURCE_DIR}/build/linux.cmake)
-endif()
-
 if(CMAKE_SIZEOF_VOID_P EQUAL 8)
     set(ARCHITECTURE x64)
 elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
     set(ARCHITECTURE x86)
 else()
     message(FATAL_ERROR "Unknown target architecture")
+endif()
+
+if(WIN32)
+    if (MSVC)
+        message(STATUS "Building with MSVC toolchain")
+    else()
+        message(FATAL_ERROR "This project requires MSVC to match precompiled libraries")
+    endif()
+    include(${PROJECT_SOURCE_DIR}/build/windows.cmake)
+else()
+    include(${PROJECT_SOURCE_DIR}/build/linux.cmake)
 endif()
 
 
